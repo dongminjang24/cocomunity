80a4bffadc1cdb4d2906024a9f17f3e09f3547bc
jangdm37 <jangdm37@gmail.com>
2024-01-10 00:31:17 +0900

    first commit

---
+++ com.cocomunity/src/test/java/hidongmin37/com/cocomunity/ApplicationTests.java
@@ -0,0 +1,13 @@
+package hidongmin37.com.cocomunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
